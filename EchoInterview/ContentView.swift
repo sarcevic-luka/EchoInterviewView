fd52622541c31d09910945529d82b45753d20926
sarcevic-luka <lukasarcevic.business@gmail.com>
2026-01-29 10:49:01 +0100

    Initial Commit

---
+++ EchoInterview/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EchoInterview
+//
+//  Created by Luka Sarcevic on 29.01.2026..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EchoInterview/EchoInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoInterviewApp.swift
+//  EchoInterview
+//
+//  Created by Luka Sarcevic on 29.01.2026..
+//
+
+import SwiftUI
+
+@main
+struct EchoInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
